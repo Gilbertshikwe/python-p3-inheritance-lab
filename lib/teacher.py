22ced8a9e879885f1b9bf8392acf8ed79591135d
Gilbert <shikweg@gmail.com>
2024-03-11 21:26:43 +0300

    Completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,12 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        if not self.knowledge:
+            return "I have no knowledge to share"
+        else:
+            return random.choice(self.knowledge)
